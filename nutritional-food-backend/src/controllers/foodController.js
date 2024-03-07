d74da900ed64a85e377200250c80675a2cb839ce
apolyanov <alexander_1256@abv.bg>
2024-03-07 17:59:40 +0200

    Adding search endpoint

--- nutritional-food-backend/src/controllers/foodController.js
+++ nutritional-food-backend/src/controllers/foodController.js
@@ -1,17 +1,27 @@
 import express from 'express';
-import { getModels } from '../db.config.js';
 import { createResponse } from '../utils.js';
+import { addFood, deleteFood, getAllFood, getFoodById, putFood, searchForFood } from '../services/foodService.js';
 
 const foodController = express.Router();
 
+foodController.get('/search', async (req, res) => {
+  const description = req.query.description;
+
+  if (description) {
+    res.status(200).json(createResponse(await searchForFood(description)));
+  } else {
+    res.status(400).json(createResponse(null, 'Bad query parameter!'));
+  }
+});
+
 foodController.get('/all', async (req, res) => {
-  const foods = await getModels().Food.findAll();
+  const foods = await getAllFood();
 
   res.status(200).json(createResponse(foods));
 });
 
-foodController.get('/:id', async (req, res) => {
-  const food = await getModels().Food.findByPk(req.params.id);
+foodController.get('/find/:id', async (req, res) => {
+  const food = await getFoodById(req.params.id);
 
   if (food) {
     res.status(200).json(createResponse(food));
@@ -20,18 +30,24 @@ foodController.get('/:id', async (req, res) => {
   }
 });
 
+foodController.put('/update/:id', async (req, res) => {
+  const updated = await putFood(req.body, req.params.id);
+
+  if (updated[0] > 0) {
+    res.status(200).json(createResponse(updated[1][0]));
+  } else {
+    res.status(404).json(createResponse(null, 'Could not update food with this ID!'));
+  }
+});
+
 foodController.post('/add', async (req, res) => {
-  const food = await getModels().Food.create({ ...req.body });
+  const food = await addFood({ ...req.body });
 
   res.status(201).json(createResponse(food, 'Food item created successfully!'));
 });
 
-foodController.delete('/:id', async (req, res) => {
-  const deletedRows = await getModels().Food.destroy({
-    where: {
-      id: req.params.id,
-    },
-  });
+foodController.delete('/delete/:id', async (req, res) => {
+  const deletedRows = await deleteFood(req.params.id);
 
   if (deletedRows > 0) {
     res.status(200).json(createResponse());
