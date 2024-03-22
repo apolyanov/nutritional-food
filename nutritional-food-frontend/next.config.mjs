f8375466bda3f4fbefe56220972aec8172808963
apolyanov <alexander_1256@abv.bg>
2024-03-22 13:48:33 +0200

    Removing console.logs

--- nutritional-food-frontend/next.config.mjs
+++ nutritional-food-frontend/next.config.mjs
@@ -3,7 +3,6 @@ const API_URL = process.env.NODE_ENV === 'development' ? process.env.DEV_API_URL
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
-        console.log(API_URL)
         return [{source: '/api/v1/:slug*', destination: `${API_URL}/v1/:slug*`}]
     },
     output: "standalone",
